b0ac0801516a7cf56fb817b472159f9d1fdbf588
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:34:59 -0600

    ej4
    Added comments explaining memory address calculations for an array.

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,11 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 4
-    return 0;
-}
+ Entre a[0] y a[3] hay 3 posiciones. 
+5012 - 500 = 12 bytes. 
+12 𝑏𝑦𝑡𝑒𝑠
+3 𝑒𝑙𝑒𝑚𝑒𝑛𝑡𝑜𝑠
+= 4 𝑏𝑦𝑡𝑒𝑠 𝑝𝑜𝑟 𝑒𝑙𝑒𝑚𝑒𝑛𝑡𝑜
+Como su tamaño es de 4 bytes, se trata de un int. 
+&𝑎[5] = 5000 + (5 · 4)
+&𝑎[5] = 5000 + 20
+&𝑎[5] = 5000 + 5020
+La fórmula general que rige este comportamiento y que permitió obtener los resultados 
+anteriores es: &a[i] = 5000 + (i * 4).
