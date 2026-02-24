e743a1eba6f060edb32688b1031e00a251300f82
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:35:53 -0600

    e5
    Added comments explaining memory allocation and size calculations.

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
-}
+a[0] = 1000 
+i = 10 
+a[10] = 1000 + 10 · 𝑡𝑎𝑚𝑎ñ𝑜 𝑑𝑒𝑙 𝑡𝑖𝑝𝑜
+a[10] = 1010 
+1010 - 1000 = 10 
+entonces  
+10 · 𝑡𝑎𝑚𝑎ñ𝑜 𝑑𝑒𝑙 𝑡𝑖𝑝𝑜 = 10
+Por lo que el tamaño del tipo debe de ser de 1 byte
