7ac87c711cb5ff1fe32c2cb0c9c255afdc3a583a
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:40:56 -0600

    e11
    Added explanation for data size and type in exercise 11.

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,5 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+Como se observa, la dirección aumenta en intervalos de 8 en 8; por lo tanto, es posible 
+determinar con seguridad que el tamaño del dato es de 8 bytes. 
+El tipo de dato correspondiente sería double, ya que es uno de los pocos tipos que utiliza 
+una extensión de 8 bytes. 
+La fórmula específica para este caso sería: 1000 +  n * 8.
