d8db3579d7c79f9d629ae34f09251095b432a5f2
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:39:52 -0600

    e9

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,7 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 9
-    return 0;
+int main() {
+int a[5];
+printf("&a[-1] = %p\n", &a[-1]);
+printf("&a[5] = %p\n", &a[5]);
+return 0;
 }
