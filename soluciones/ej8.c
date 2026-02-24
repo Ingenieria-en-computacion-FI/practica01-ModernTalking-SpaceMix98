32a2323124cedf2ea1258c8e83748ce32354fcff
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:38:56 -0600

    e8
    Added calculations and comments for exercise 8.

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 8
-    return 0;
-}
+2024 = 2000 + (1 * N + 2) * 4  
+24 = (N + 2) * 4 
+6 = N + 2 
+N = 4 columnas 
+Con 2 filas (M=2), con el índice 1 existe: 
+Tamaño = M * N * tamaño(int)  
+Tamaño = 2 * 4 * 4 = 32 bytes 
+por lo que es de 32 bytes 
