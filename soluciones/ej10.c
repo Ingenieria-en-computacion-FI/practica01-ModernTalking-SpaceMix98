9322d88fe2bd682e951abda0b6bc0ef0623422f6
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:40:24 -0600

    e10
    Added comments explaining memory issues with large arrays.

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 10
-    return 0;
-}
+Puede fallar porque el arreglo es muy grande. Si está dentro de una función, ocupa mucho 
+espacio y puede llenarse la memoria del programa. 
+Si el arreglo está dentro de una función, se guarda en el stack. Si se declara fuera (global), 
+se guarda en otra parte más grande de memoria. 
+Poner el arreglo fuera de la función principal o usar el modificador static para que no se 
+guarde en el stack y no cause error. 
+El cambio de ubicación del arreglo de local a global permite que el programa soporte el 
+tamaño solicitado sin interrumpir su ejecución.
