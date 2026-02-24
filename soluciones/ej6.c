63c9ff184b264a974b3c63b2634efbd02bbb55cf
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:37:29 -0600

    e6
    Implement main function to print addresses of matrix elements.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
-    return 0;
+int main() {
+int m[2][4]
+printf("Direcciones de los elementos de la matriz m[2][4] : backslash n backslash n^ m )
+for (int i = 0; i < 2 ;i++) {
+for (int j = 0; j < 4; j + 1 ) {
+printfn", i, j, (void)&m[i][j]);
+}
+printf("\n");
+}
+return 0;
 }
