efb83bf599a0704f4e0fcbf17484df8bbd21244c
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:41:34 -0600

    e12
    Added analysis and results for exercise 12 in comments.

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 12
-    return 0;
-}
+A grandes rasgos, el valor resultante sería 4056; sin embargo, al realizar el análisis correcto 
+bajo el sistema hexadecimal, el valor obtenido es 403A. 
+En este caso, el resultado sería 4032. Al igual que en el punto anterior, si lo convertimos a 
+hexadecimal, el valor final queda representado como 4020. 
+La diferencia es de 24 bytes. Este cálculo se obtiene gracias al conocimiento del "peso" o 
+tamaño que ocupa en memoria un dato de tipo entero. 
+Este cálculo confirma que el sistema linealiza la matriz multiplicando el índice de fila por el 
+número total de columnas (N = 5) antes de sumar el índice de columna.
