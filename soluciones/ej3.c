fa6889de24d986bfcaa83b1ed63a77c0d0bbbadf
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 15:43:32 -0600

    ej3

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,24 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 3
-    return 0;
+int main() {
+int a[3];
+int b[3];
+int c[3];
+printf("=== Direcciones del arreglo a ===\n");
+for(int i=0; i<3; i++) {
+
+printf("&a[%d] = %p\n", i, &a[i]);
+}
+printf("=== Direcciones del arreglo b ====\n");
+for(int i=0; i<3; i++) {
+
+printf("&b[%d] = %p\n", i, &b[i]);
+}
+printf("=== Direcciones del arreglo c ===\n");
+for(int i=0; i<3; i++) {
+
+printf("&c [%d] = %p\n", i, &c[i]);
+
+}
+return 0;
+
 }
