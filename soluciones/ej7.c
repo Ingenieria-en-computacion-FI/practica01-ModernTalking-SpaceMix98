0b635d332bcdd43fc4dc4e7e2edb6a5b20f44a1c
ModernTalking-SpaceMix98 <koprcatgurlxdxdxdxd@gmail.com>
2026-02-24 16:38:11 -0600

    e7
    Add explanation about memory addresses in 2D arrays.

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,4 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 7
-    return 0;
-}
+Dirección &m[0][3] = base + (0 * N + 3) * S = base + 3S 
+Dirección &m[1][0] = base + (1 * N + 0) * S = base + NS 
+La dirección → m[1][0] es mayor que[0][3] siempre que el número de columnas (N) sea 
+mayor a 3.  
